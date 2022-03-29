908ff23f65f03565921ea5b0f60d906212ffb84d
hanni66 <haeunkim0807@naver.com>
2022-03-29 19:40:08 +0900

    hw10_1, 2

--- hw10_1/src/LamdaTest.java
+++ hw10_1/src/LamdaTest.java
@@ -1,20 +1,19 @@
 //***************************
 // 파일명: LamdaTest.java
 // 작성자: 김하은
-// 작성일: 2022.03.28
+// 작성일: 2022.03.29
 // 내용: 람다식 연습
 //***************************
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+//import java.awt.event.ActionListener;
 import javax.swing.Timer;
 
 public class LamdaTest {
 	public static void main(String[] args) {
-		Timer t = new Timer(1000, new ActionListener() {
-			public void actionPerformed(ActionEvent event) {
-				System.out.println("beep");
-			}
-		});
+		System.out.println("hw10_1 : 김하은");
+		Timer t = new Timer(1000, 
+				(ActionEvent event) -> {System.out.println("beep");}
+		);
 		t.start();
 		for (int i = 0; i < 1000; i++) {
 			try {
@@ -22,5 +21,9 @@ public class LamdaTest {
 			}
 			catch (InterruptedException e) {}
 		}
+	}
+	
+	public void actionPerformed(ActionEvent event) {
+		System.out.println("beep");
 	}
 }
--- hw10_2/src/LamdaTest.java
+++ hw10_2/src/LamdaTest.java
@@ -1,4 +1,27 @@
+//***************************
+// 파일명: LamdaTest.java
+// 작성자: 김하은
+// 작성일: 2022.03.29
+// 내용: 두개의 정수를 받아서 두개의 정수를 곱한 값을 반환하는 프로그램 작성.
+//***************************
+import java.util.Scanner;
+
+interface Multi {
+	int multiply(int x, int y);
+}
 
 public class LamdaTest {
-
-}
+	public static void main(String[] args) {
+		System.out.println("hw10_2 : 김하은");
+		Multi m = (x, y) -> {return x * y;};
+		Scanner sc = new Scanner(System.in);
+		int x;
+		int y;
+		
+		x = sc.nextInt();
+		y= sc.nextInt();
+		System.out.println(m.multiply(x, y));
+		
+		sc.close();
+	}
+}  
